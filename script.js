fdd708b9e82a09641fd5158ce017034d4774c2ac
Grazielly Avelino <graziellyavelino2@gmail.com>
2025-07-01 19:52:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+  const mensagem = "A jornada é feita de pequenos começos. Que este seja apenas um deles.";
+  document.getElementById("mensagem").innerText = mensagem;
+}
